2840cd8712fdfd1541412652c1096f3feec145df
TitouanRobert <TitouanRobert@users.noreply.github.com>
2025-03-19 17:41:26 +0100

    add mypy

--- lib/my_function.py
+++ lib/my_function.py
@@ -1,10 +1,10 @@
 import numpy
 
 
-def to_test(a):
+def to_test(a: int) -> int:
 
     return a + 1
 
 
-def numpy_vect():
+def numpy_vect() -> numpy.ndarray:
     return numpy.array([1, 2, 3])
--- tests/test_my_function.py
+++ tests/test_my_function.py
@@ -1,9 +1,9 @@
 from lib import numpy_vect, to_test
 
 
-def test_to_test():
+def test_to_test() -> None:
     assert to_test(5) == 6
 
 
-def test_numpy():
+def test_numpy() -> None:
     assert numpy_vect().sum() == 6
